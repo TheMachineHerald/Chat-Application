da71457aa6d9999c4fd71ea59dd7353f79fe0d13
Machine Herald <spike@pop-os.localdomain>
2021-10-12 23:45:15 -0400

    adds mysql connection pool

---
+++ src/database/connection.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql'
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    multipleStatements: true
+})
+export default connection
