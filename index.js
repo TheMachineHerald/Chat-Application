4820c12ba2516dc5e7d499f66d7ee635b1e7d674
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-13 03:36:34 -0400

    adds database connection, database utils, login route and adds urlencoding and cors to app

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
-import express, { json } from 'express'
+import express, { json, urlencoded } from 'express'
+import cors from 'cors'
 import API from './src/routes'
+import connection from './src/database/connection'
 
 const app = express()
 const port = process.env.PORT || 3000
 
+global.db_connection = connection
+
 app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(cors())
 app.use('/api', API)
 
 app.get('/', (req, res) => {
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return <h1>Hello World!</h1>
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
