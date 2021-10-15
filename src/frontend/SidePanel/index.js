6f4a96a4a2acb4adcc1d118a6850a6622a4491b5
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-15 02:29:03 -0400

    edits side panel component

--- src/frontend/SidePanel/index.js
+++ src/frontend/SidePanel/index.js
@@ -5,7 +5,11 @@ function SidePanel() {
   return (
     <div className="side-panel">
       <h2>I am the Side Panel</h2>
-      
+
+      <div className="side-panel-top">
+
+      </div>
+
     </div>
   )
 }
