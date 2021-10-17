442529db5c62b79f4012ef3998aed4cbb6893d52
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-17 12:32:35 -0400

    adds register form

--- src/frontend/Components/Login/Components/RegisterModal/index.js
+++ src/frontend/Components/Login/Components/RegisterModal/index.js
@@ -1,10 +1,76 @@
 import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import styles from './RegisterModal.module.scss'
 
 function RegisterModal() {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values)
+  }
   return (
     <div className={styles.registerModal}>
-      Register
+      <h2 className={styles.createTitle}>Create Account</h2>
+
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email Address!',
+            },
+          ]}
+        >
+          <h4 className={styles.inputTitles}>Email</h4>
+          <Input
+            className={styles.inputDiscord}
+          />
+        </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <h4 className={styles.inputTitles}>Username</h4>
+          <Input
+            className={styles.inputDiscord}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <h4 className={styles.inputTitles}>Password</h4>
+          <Input
+            className={styles.inputDiscord}
+            type="password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className={styles.button}>
+            Continue
+          </Button>
+        </Form.Item>
+
+        <span className={styles.login}><a href="">Already have an account?</a></span>
+
+      </Form>
     </div>
   )
 }
