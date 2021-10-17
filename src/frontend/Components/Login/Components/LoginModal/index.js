a5838da10abbc9a8aca23fb7024050c005d1554e
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-17 11:59:09 -0400

    adds login form, styles to match discord

--- src/frontend/Components/Login/Components/LoginModal/index.js
+++ src/frontend/Components/Login/Components/LoginModal/index.js
@@ -1,23 +1,65 @@
 import React from 'react'
-import { Button } from 'antd'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import styles from './LoginModal.module.scss'
 
 function LoginModal() {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values)
+  }
   return (
     <div className={styles.loginModal}>
-      <h2 className={styles.fontLight}>Welcome back!</h2>
-      <h3 className={styles.fontLight}>We're so exicted to see you again!</h3>
+      <h2 className={styles.fontLighter}>Welcome back!</h2>
+      <h3 className={styles.fontSilver}>We're so exicted to see you again!</h3>
 
-      <h4 className={styles.fontLight}>EMAIL</h4>
-      <input />
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email Address!',
+            },
+          ]}
+        >
+          <Input
+            className={styles.inputDiscord}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            className={styles.inputDiscord}
+            type="password"
+          />
+          <a className={styles.link} href="">
+            Forgot password?
+          </a>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className={styles.button}>
+            Log in
+          </Button>
+        </Form.Item>
 
-      <h4 className={styles.fontLight}>Password</h4>
-      <input />
-      <p className={styles.fontBlue}>Forgot your password?</p>
-
-      <Button className={styles.button}>Login</Button>
-
-      <p className={styles.fontDark}>Need an account? <span className={styles.fontBlue}>Register</span></p>
+        <p className={styles.fontDark}>Need an account?
+          <span className={styles.register}><a href="">Register</a></span>
+        </p>
+      </Form>
     </div>
   )
 }
