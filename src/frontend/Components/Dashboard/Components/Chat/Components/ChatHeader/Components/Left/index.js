eab9add43d33cc5e7ea4bdcc4310e4939fb6191e
Machine Herald <canthandlethetruth01@gmail.com>
2021-10-27 12:43:28 -0400

    replaces text hash with icon hash

--- src/frontend/Components/Dashboard/Components/Chat/Components/ChatHeader/Components/Left/index.js
+++ src/frontend/Components/Dashboard/Components/Chat/Components/ChatHeader/Components/Left/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { NumberOutlined } from '@ant-design/icons'
 import styles from './Left.module.scss'
 
 function Left (props) {
   return (
     <div className={styles.left}>
       <h3>
-        <span className={styles.hash}>#</span>
+        <NumberOutlined className={styles.hash}/>
         {props.channel}
       </h3>
     </div>
