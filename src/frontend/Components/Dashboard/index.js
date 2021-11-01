b96a7f76b8a0f3e6d98003429694b6e8a8dcfde8
Machine Herald <canthandlethetruth01@gmail.com>
2021-11-01 01:57:22 -0400

    adds toggle to channel user list

--- src/frontend/Components/Dashboard/index.js
+++ src/frontend/Components/Dashboard/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, createContext } from "react"
 import { useSelector } from "react-redux"
 import { ServersContainer } from "./Components/ServersContainer"
 import SidePanel from "./Components/SidePanel"
-import Chat from "./Components/Chat"
+import { Chat } from "./Components/Chat"
 import Barebones_Socket from "../../websocket"
 import styles from "./Dashboard.module.scss"
 
