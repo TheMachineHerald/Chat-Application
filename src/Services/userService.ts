433391cdf04b44a222af928380724b222038fa41
Machine Herald <canthandlethetruth01@gmail.com>
2021-11-06 23:06:13 -0400

    refactors user server to ts and adds lots of types

--- src/Services/userService.ts
+++ src/Services/userService.ts
@@ -4,7 +4,7 @@ import {
 	handleResponse,
 	handleLoginResponse
 } from "./util"
-// API_URL-- just for dev
+
 const API_LINK = config().API_URL
 
 export const userService = {
@@ -20,7 +20,7 @@ export const userService = {
 	saveMessage
 }
 
-function login(email, password) {
+function login(email: string, password: string): Promise<LOGIN_ROUTE_RESPONSE> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -29,8 +29,7 @@ function login(email, password) {
 
 	return fetch(`${API_LINK}/api/login`, requestOptions)
 		.then(handleLoginResponse)
-		.then(response => {
-			if (response === -1) return logout()
+		.then((response: LOGIN_ROUTE_RESPONSE): LOGIN_ROUTE_RESPONSE => {
 			if (response.payload.user) {
 				response.payload.user.authdata = window.btoa(email + ":" + password)
 				localStorage.setItem("chat_user", JSON.stringify(response.payload.user))
@@ -39,7 +38,7 @@ function login(email, password) {
 		})
 }
 
-function register(register_obj) {
+function register(register_obj: REGISTER_OBJECT): Promise<REGISTERED_USER> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -48,14 +47,14 @@ function register(register_obj) {
 
 	return fetch(`${API_LINK}/api/register`, requestOptions)
 		.then(handleResponse)
-		.then(user => {
+		.then((user: REGISTERED_USER): REGISTERED_USER => {
 			user.authdata = window.btoa(user.email + ":" + user.passwrd)
 			localStorage.setItem("chat_user", JSON.stringify(user))
 			return user
 		})
 }
 
-function logout(user_id) {
+function logout(user_id: string | number): Promise<void> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -64,7 +63,7 @@ function logout(user_id) {
 
 	return fetch(`${API_LINK}/api/logout`, requestOptions)
 		.then(handleResponse)
-		.then(resolve => {
+		.then((resolve: void) => {
 			localStorage.removeItem("chat_user")
 			localStorage.clear()
 		})
@@ -74,12 +73,12 @@ function logout(user_id) {
 		})
 }
 
-function clearCache() {
+function clearCache(): void {
 	localStorage.removeItem("chat_user")
 	localStorage.clear()
 }
 
-function getAllChannels(user_id) {
+function getAllChannels(user_id: string | number): Promise<CHANNELS[]> {
 	const requestOptions = {
 		method: "GET",
 		headers: authHeader()
@@ -87,12 +86,12 @@ function getAllChannels(user_id) {
 
 	return fetch(`${API_LINK}/api/channels/${user_id}`, requestOptions)
 		.then(handleResponse)
-		.then(channels => {
+		.then((channels: Array<CHANNELS>): CHANNELS[] => {
 			return channels
 		})
 }
 
-function getChannelMessages(channel_id) {
+function getChannelMessages(channel_id: string | number): Promise<CHANNEL_MESSAGES[]> {
 	const requestOptions = {
 		method: "GET",
 		headers: authHeader()
@@ -100,12 +99,13 @@ function getChannelMessages(channel_id) {
 
 	return fetch(`${API_LINK}/api/channels/messages/${channel_id}`, requestOptions)
 		.then(handleResponse)
-		.then(messages => {
+		.then((messages: Array<CHANNEL_MESSAGES>): CHANNEL_MESSAGES[] => {
 			return messages
 		})
 }
 
-function getAllUserFriends() {
+//define later
+function getAllUserFriends(): Promise<any> {
 	const requestOptions = {
 		method: "GET",
 		headers: authHeader()
@@ -113,12 +113,12 @@ function getAllUserFriends() {
 
 	return fetch(`${API_LINK}/api/friends`, requestOptions)
 		.then(handleResponse)
-		.then(friends => {
+		.then((friends: any): any => {
 			return friends
 		})
 }
 
-function saveSelectedChannel(ctx) {
+function saveSelectedChannel(ctx: SAVE_SELECTED_CHANNEL_OBJECT): Promise<_SAVE_SELECTED_CHANNEL_PAYLOAD> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -127,12 +127,12 @@ function saveSelectedChannel(ctx) {
 
 	return fetch(`${API_LINK}/api/channels/save-selected-channel`, requestOptions)
 		.then(handleResponse)
-		.then(response => {
+		.then((response: _SAVE_SELECTED_CHANNEL_PAYLOAD): _SAVE_SELECTED_CHANNEL_PAYLOAD => {
 			return response
 		})
 }
 
-function saveSelectedServer(ctx) {
+function saveSelectedServer(ctx: SAVE_SELECTED_SERVER_OBJECT): Promise<SELECTED_SERVER> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -141,12 +141,12 @@ function saveSelectedServer(ctx) {
 
 	return fetch(`${API_LINK}/api/servers/save-selected-server`, requestOptions)
 		.then(handleResponse)
-		.then(response => {
+		.then((response: SELECTED_SERVER): SELECTED_SERVER => {
 			return response
 		})
 }
 
-function saveMessage(ctx) {
+function saveMessage(ctx: SAVE_MESSAGE_OBJECT): Promise<void> {
 	const requestOptions = {
 		method: "POST",
 		headers: { "Content-Type": "application/json" },
@@ -155,7 +155,7 @@ function saveMessage(ctx) {
 
 	return fetch(`${API_LINK}/api/channels/save-message`, requestOptions)
 		.then(handleResponse)
-		.then(response => {
+		.then((response: void): void => {
 			return response
 		})
 }
