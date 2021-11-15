ca19eee751cebea6b1c8b197a6e5c31b5645693d
Machine Herald <canthandlethetruth01@gmail.com>
2021-11-15 02:58:44 -0500

    es lint

--- src/Components/Dashboard/Components/SidePanel/Components/FriendsContainer/components/SidePanelUser/index.tsx
+++ src/Components/Dashboard/Components/SidePanel/Components/FriendsContainer/components/SidePanelUser/index.tsx
@@ -15,13 +15,10 @@ const SidePanelUser: React.FC<SIDE_PANEL_USER_PROPS> = (props): ReactElement => 
 		}
 		dispatch({ type: "SAVE_FRIEND_PAGE", payload: false })
 		dispatch({ type: "SAVE_PAGE_SELECTION", payload: "FRIEND" })
-		dispatch({
-			type: "SAVE_SELECTED_FRIEND",
-			payload: {
-				selected_friend_id: ctx.friend_id,
-				selected_friend_user_name: ctx.friend_user_name
-			}
-		})
+		dispatch({ type: "SAVE_SELECTED_FRIEND", payload: {
+			selected_friend_id: ctx.friend_id,
+			selected_friend_user_name: ctx.friend_user_name
+		}})
 
 		return (
 			Promise.all([
